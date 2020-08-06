7913cb0cce3b3cf4acf03827ed4ab1a36af12b2d
Ryan Chen <chenrkc030@gmail.com>
2020-08-05 22:58:34 -0400

    Organized contents by platforming code

---
+++ Python Code/Webscraping/test_function_Prerequisites.py
@@ -0,0 +1,26 @@
+import requests
+from bs4 import BeautifulSoup
+from csv import writer
+import re
+
+link = 'https://oscar.gatech.edu/pls/bprod/bwckschd.p_disp_detail_sched?term_in=202008&crn_in=88781'
+
+
+response = requests.get(link)           
+soup = BeautifulSoup(response.text, 'html.parser')      #get the raw html from the link in text form
+
+#get the "Prerequisites" from the page
+body = soup.find(class_='dddefault').text
+
+##########make edge case if there are no prereqs like CRN 80087#################################################
+# try: 
+rawPrereqs = body.lower().rsplit('prerequisites:')[1]
+# except:
+
+################ filter out excess text to return only course numbers ##########
+rawPrereqs = re.sub('\sminimum\sgrade\sof\s.','',rawPrereqs)
+
+# rawPrereqs = rawPrereqs.replace(' minimum grade of c', '').replace(' minimum grade of d', '').replace(' minimum grade of t', '')
+rawPrereqs = rawPrereqs.replace('undergraduate semester level  ','')
+
+print(rawPrereqs)
